ae36d2a8b77871fd27bc98dd80f566bfc880e565
Karoline <kkohlsstangherlin@gmail.com>
2023-03-14 17:02:14 -0300

    :tada: Iniciando projeto Talker Manager

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = process.env.PORT || '3001';
+const PORT = process.env.PORT || "3001";
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
+app.get("/", (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
 app.listen(PORT, () => {
-  console.log('Online');
+  console.log("Servidor está rodando na pota 3001");
 });
